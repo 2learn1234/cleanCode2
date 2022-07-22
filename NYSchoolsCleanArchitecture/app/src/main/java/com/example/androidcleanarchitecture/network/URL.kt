13641105702f2f5dccf9c4edb4b24242c3bcdfcc
Firas <2learn1234@gmail.com>
2022-07-21 17:36:39 -0700

    added gradle

---
+++ NYSchoolsCleanArchitecture/app/src/main/java/com/example/androidcleanarchitecture/network/URL.kt
@@ -0,0 +1,9 @@
+package com.example.androidcleanarchitecture.network
+
+import com.example.androidcleanarchitecture.BuildConfig
+
+object URL {
+     const val baseURL: String = BuildConfig.BASE_URL
+     const val GET_NEWS = "s3k6-pzi2.json"
+     const val GET_NEWS2 = "f9bf-2cp4.json"
+}
